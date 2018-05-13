be3f4a4f248c28ef3c3f1a2e79402f4266d86163
anabg <anitagrimaut@gmail.com>
2018-05-13 10:43:04 -0300

    add classes to connecto with mysql

---
+++ src/main/java/com/start/springdatawithmysql/Application.java
@@ -0,0 +1,15 @@
+package com.start.springdatawithmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ÊAnita on 13/5/2018.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/start/springdatawithmysql/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.start.springdatawithmysql.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.start.springdatawithmysql.entities.User;
+
+/**
+ * Created by ÊAnita on 13/5/2018.
+ */
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
