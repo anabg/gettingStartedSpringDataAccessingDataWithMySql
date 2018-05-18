f7d62f02262ac7f21fabc86f8d0d4ace1015cc4d
Grimaut, Ana <Ana_Grimaut@McAfee.com>
2018-05-18 18:33:00 -0300

    add changes - with this changes the query is good and works with multiple fetchs and filters without duplicates

--- src/main/java/com/start/springdatawithmysql/repositories/CustomUserRepository.java
+++ src/main/java/com/start/springdatawithmysql/repositories/CustomUserRepository.java
@@ -4,5 +4,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 
 public interface CustomUserRepository {
 
-    public CriteriaBuilder getQuery(final String filter);
+    //public CriteriaBuilder getQuery(final String filter);
+    public CriteriaBuilder getQuery(final String filter, final Class<?> o);
+
 }
--- src/main/java/com/start/springdatawithmysql/repositories/CustomUserRepositoryImpl.java
+++ src/main/java/com/start/springdatawithmysql/repositories/CustomUserRepositoryImpl.java
@@ -4,9 +4,10 @@ import com.start.springdatawithmysql.entities.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.*;
-import java.util.Arrays;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class CustomUserRepositoryImpl implements CustomUserRepository {
 
@@ -20,7 +21,7 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
 
     private String[] getKey(String value){
         String[ ] keys = new String[2];
-        String k = value.replace(" ", "").contains("$id") ? value.replace("$id","id") : value;
+        String k = value.replace(" ", "").contains("$id") ? value.replace("$id","id").toLowerCase() : value.toLowerCase();
 
         if(k.contains(".")){
             keys = k.split("\\.");
@@ -86,10 +87,11 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
     }
 
 
-    public CriteriaBuilder getQuery(final String filter) {
+    public CriteriaBuilder getQuery(final String filter, final Class<?> className) {
 
-        int count = 0;
-        StringBuilder fromQuery = new StringBuilder("Select s from User s ");
+        String entityName = className.getSimpleName();
+        StringBuilder query = new StringBuilder("Select " + className.getSimpleName().toLowerCase().substring(0,2) + " from " + entityName + " " + entityName.substring(0,2).toLowerCase());
+        Set<String> fromQuery = new HashSet<>();
         StringBuilder where = new StringBuilder();
         String[] splitedQuery = null;
         String value = "";
@@ -106,31 +108,74 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
 
             if(key.length > 1){
 
-                alias = getQueryWithMultipleFetchs(fromQuery, where, alias, value, key);
+                alias = getQueryWithMultipleFetchs(className, fromQuery, where, alias, value, key);
 
             } else {
-                where.append(" where s." + key[0] + " = " + "'" + value + "'");
+                where.append(" where " + entityName.substring(0,2) +"." + key[0] + " = " + "'" + value + "'");
             }
 
         }
 
-         String completeQuery = fromQuery.toString() + where.toString();
-         entityManager.createQuery(completeQuery, User.class).getResultList();
+        for(String s : fromQuery ){
+            query.append(s);
+        }
+
+
+         String completeQuery = query.toString() + where.toString();
+         entityManager.createQuery(completeQuery, className).getResultList();
 
         return null;
 
     }
 
-    private String getQueryWithMultipleFetchs(StringBuilder fromQuery, StringBuilder where, String alias, String value, String[] key) {
+    private String getQueryWithMultipleFetchs(final Class<?> className, final Set<String> fromQuery, final StringBuilder where, final String alias, final String value, final String[] key) {
+        String a = alias;
+        for(int i = 0; i <= key.length-2 ; i++){
+            a = key[i].substring(0,2);
+            String keyValue = key[i] + " " + a;
+
+                if(i==0){
+                    fromQuery.add(" join fetch " + className.getSimpleName().substring(0,2).toLowerCase() + "." + keyValue);
+                } else {
+                    fromQuery.add(" join fetch " + key[i-1].substring(0,2) + "." + keyValue);
+                }
+        }
+
+        String whereValue = a + "." + key[key.length-1];
+        if(where.length()==0){
+            where.append(" where " + whereValue );
+        } else {
+            where.append(" and " + whereValue);
+        }
+
+        where.append(" = " + "'" + value + "'");
+        return a;
+    }
+
+
+    /*private String getQueryWithMultipleFetchs2(Class<?> className, StringBuilder fromQuery, StringBuilder where, String alias, String value, String[] key) {
+        List<String> fetchs = new ArrayList<String>();
         for(int i = 0; i <= key.length-2 ; i++){
             alias = key[i].substring(0,2);
             String keyValue = key[i] + " " + alias;
+
+
             if(i==0){
-                fromQuery.append(" join fetch s." + keyValue);
+                fromQuery.append(" join fetch " + className.getSimpleName().substring(0,2) + "." + keyValue);
+                //fetchs.add(" join fetch " + className.getSimpleName().toLowerCase().substring(0,2) + "." + keyValue);
             } else {
                 fromQuery.append(" join fetch " + key[i-1].substring(0,2) + "." + keyValue);
+                //fetchs.add(" join fetch " + key[i-1].substring(0,2) + "." + keyValue);
             }
         }
+
+        *//*List<String> fetchsWithoutDuplicates = fetchs.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        for(String s : fetchsWithoutDuplicates ){
+            fromQuery.append(s);
+        }*//*
 
         String whereValue = alias + "." + key[key.length-1];
         if(where.length()==0){
@@ -141,15 +186,16 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
 
         where.append(" = " + "'" + value + "'");
         return alias;
-    }
+    }*/
 
-    private String getValue(String pattern , String value) {
+    private String getValue(final String pattern ,final String value) {
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(value);
+        String result = "";
 
         while (m.find()) {
-            value = m.group();
+            result = m.group();
         }
-        return value;
+        return result;
     }
 }
